71587b5ad7e0dfd6ddc97bcc28624b2c96a275a5
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2020-10-22 23:53:53 +0500

    Выполняет задание

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
 'use strict';
 
-const User_Number = [`01`, `02`, `03`, `04`, `05`, `06`, `07`, `08`];
+const PIN_WIDTH = 50;
+const PIN_HEIGHT = 70;
 
-const locationX;
+const TYPE = [`palace`, `flat`, `house`, `bungalow`];
 
-const locationY;
+const CHECKIN = [`12:00`, `13:00`, `14:00`];
 
-const Type = [`palace`, `flat`, `house`, `bungalow`];
+const CHECKOUT = [`12:00`, `13:00`, `14:00`];
 
-const Checkin = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 
-const Checkout = [`12:00`, `13:00`, `14:00`];
-
-const Features = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-
-const Photos = [
+const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
@@ -27,32 +24,72 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const createSimilarAdverts = (author, offer, location) => {
-  let similarAdverts = [];
-  for (let i = 0; i < 8; i++) {
+const TITLES = [
+  `Title1`,
+  `Title2`,
+  `Title3`
+];
 
-    similarAdverts[i] = {
-      "author": {
-        "avatar": src = `../img/avatars/userUser_Number[i].png`
-      },
-      "offer": {
-        "title": title,
-        "address": locationX, locationY,
-        "price": price,
-        "type": Type,
-        "rooms": rooms,
-        "guests": guests,
-        "checkin": Checkin,
-        "checkout": Checkout,
-        "features": Features,
-        "description": description,
-        "photos": Photos
-      },
-      "location": {
-        "x": getRandomInt(0, max),
-        "y": getRandomInt(130, 630)
-      }
-    };
+const DESCRIPTIONS = [
+  `Description1`,
+  `Description2`,
+  `Description3`
+];
+
+const PRICE_MIN = 500;
+const PRICE_MAX = 200000;
+
+const createSimilar = (num) => {
+  const features = [];
+  let temp = FEATURES.slice();
+  let size = getRandomInt(0, FEATURES.length);
+
+  for (let i = 0; i < size; i++) {
+    const index = getRandomInt(0, temp.length);
+    features.push(temp[index]);
+    temp.splice(index, 1);
+  }
+
+  const photos = [];
+  temp = PHOTOS.slice();
+  size = getRandomInt(0, PHOTOS.length);
+
+  for (let i = 0; i <= size; i++) {
+    const index = getRandomInt(0, temp.length);
+    photos.push(temp[index]);
+    temp.splice(index, 1);
+  }
+
+  const similarAdvert = {
+    "author": {
+      "avatar": `./img/avatars/user0${num}.png`
+    },
+    "offer": {
+      "title": TITLES[getRandomInt(0, TITLES.length)],
+      "address": `${getRandomInt(0, 1000)}, ${getRandomInt(0, 1000)}`,
+      "price": getRandomInt(PRICE_MIN, PRICE_MAX),
+      "type": TYPE[getRandomInt(0, TYPE.length)],
+      "rooms": getRandomInt(1, 4),
+      "guests": getRandomInt(2, 12),
+      "checkin": CHECKIN[getRandomInt(0, CHECKIN.length)],
+      "checkout": CHECKOUT[getRandomInt(0, CHECKOUT.length)],
+      "features": features,
+      "description": DESCRIPTIONS[getRandomInt(0, DESCRIPTIONS.length)],
+      "photos": photos
+    },
+    "location": {
+      "x": getRandomInt(0, 1200),
+      "y": getRandomInt(130, 630)
+    }
+  };
+  return similarAdvert;
+};
+
+const createSimilarAdverts = () => {
+  let similarAdverts = [];
+
+  for (let i = 1; i < 9; i++) {
+    similarAdverts.push(createSimilar(i));
   }
   return similarAdverts;
 };
@@ -60,14 +97,12 @@ const createSimilarAdverts = (author, offer, location) => {
 const adsMap = document.querySelector(`.map`);
 adsMap.classList.remove(`map--faded`);
 
-const adPinTemplate = document.querySelector(`#pin`).content;
-
-const adPin = adPinTemplate.querySelector(`.map__pin`);
+const adPinTemplate = document.querySelector(`#pin`).content.firstElementChild;
 
 const renderAdPin = (advert) => {
-  const adPinElement = adPin.cloneNode(true);
+  const adPinElement = adPinTemplate.cloneNode(true);
 
-  adPinElement.style.style = `left: location.x + смещение по X + `px`; top: location.y + смещение по Y + `px`;`;
+  adPinElement.style = `left: ${advert.location.x - PIN_WIDTH/2}px; top: ${advert.location.y - PIN_HEIGHT}px;`;
   adPinElement.querySelector(`img`).src = advert.author.avatar;
   adPinElement.querySelector(`img`).alt = advert.offer.title;
 
@@ -75,8 +110,9 @@ const renderAdPin = (advert) => {
 };
 
 const fragment = document.createDocumentFragment();
-for (let i = 0; i < createSimilarAdverts(author, offer, location).length; i++) {
-  fragment.appendChild(renderAdPin(createSimilarAdverts(author, offer, location)[i]));
+const adverts = createSimilarAdverts();
+for (let i = 0; i < adverts.length; i++) {
+  fragment.appendChild(renderAdPin(adverts[i]));
 }
 
 const mapPins = document.querySelector(`.map__pins`);
